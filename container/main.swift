a803fa5ea4c9a90a6adf3add988c59854a2be3b6
Mickey Barboi <damouse007@gmail.com>
2015-09-24 21:55:13 -0500

    Initial Commit

---
+++ container/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  container
+//
+//  Created by Mickey Barboi on 9/24/15.
+//  Copyright © 2015 paradrop. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
