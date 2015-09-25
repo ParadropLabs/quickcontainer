fec66f710ea9188cced7928b4f64afeb72a03137
Mickey Barboi <damouse007@gmail.com>
2015-09-24 22:37:24 -0500

    run loops are the answer

---
+++ container/container-Briding-Header.h
@@ -0,0 +1,14 @@
+//
+//  container-Briding-Header.h
+//  container
+//
+//  Created by Mickey Barboi on 9/24/15.
+//  Copyright © 2015 paradrop. All rights reserved.
+//
+
+#ifndef container_Briding_Header_h
+#define container_Briding_Header_h
+
+#import "MDWamp.h"
+
+#endif /* container_Briding_Header_h */
--- container/main.swift
+++ container/main.swift
@@ -9,5 +9,76 @@
 import Foundation
 import Cocoa
 
-print("Hello, World!")
+print("Starting Container")
+
+
+//let URL = "ws://localhost:8000/ws"
+
+class RiffleSession: NSObject, MDWampClientDelegate {
+    var socket: MDWampTransportWebSocket?
+    var session: MDWamp?
+    
+    override init() {
+        super.init()
+        print("Attempting to connect...")
+
+        socket = MDWampTransportWebSocket(server:NSURL(string: "ws://localhost:8000/ws"), protocolVersions:[kMDWampProtocolWamp2msgpack, kMDWampProtocolWamp2json])
+        session = MDWamp(transport: socket, realm: "pd.damouse", delegate: self)
+        
+    }
+    
+    func connect() {
+        session?.connect()
+    }
+    
+    func mdwamp(wamp: MDWamp!, sessionEstablished info: [NSObject : AnyObject]!) {
+        print("Session Established!")
+    }
+    
+    func mdwamp(wamp: MDWamp!, closedSession code: Int, reason: String!, details: [NSObject : AnyObject]!) {
+        print("Session Closed!")
+    }
+    
+}
+
+let s = RiffleSession()
+
+
+// Attempt 1 to get the program to block
+//let oldify = NSBlockOperation() {
+//    NSThread.sleepForTimeInterval(10)
+//    NSLog("Sleep finished")
+//}
+//
+//let queue = NSOperationQueue()
+//queue.addOperations([oldify], waitUntilFinished: true)
+
+// Attempt 2
+
+//dispatch_async(dispatch_get_global_queue(0, 0)) { () -> Void in
+//    let s = RiffleSession()
+//}
+//
+//NSThread.sleepForTimeInterval(2)
+
+
+
+// Attempt 3
+//let qualityOfServiceClass = QOS_CLASS_BACKGROUND
+//let backgroundQueue = dispatch_get_global_queue(qualityOfServiceClass, 0)
+//dispatch_async(backgroundQueue, {
+//    s.connect()
+//    
+//    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//        print("This is run on the main queue, after the previous code in outer block")
+//    })
+//})
+//
+//NSThread.sleepForTimeInterval(2)
+
+// Attempt 4
+
+
+print("Done")
+
 
